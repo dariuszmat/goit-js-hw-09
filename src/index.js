1b7f6d81872798e7f5e28296ca88262d3fa940bc
Dariusz Matyniak <dariusz.cie@gmail.com>
2023-11-18 20:45:59 +0100

    third commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const bodyElement = document.body;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let intervalId;
+
+
+startButton.addEventListener('click', () => {
+
+ startButton.disabled = true;
+
+  intervalId = setInterval(() => {
+    bodyElement.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+
+stopButton.addEventListener('click', () => {
+
+  startButton.disabled = false;
+  
+  clearInterval(intervalId);
+});
